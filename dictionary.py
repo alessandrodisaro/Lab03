ed0c856779e3dba7566b154f8306a18cb52ee464
alessandrodisaro <alexajd@libero.it>
2024-03-21 13:29:02 +0100

    completato il progetto aggiungendo il supporto per lo spagnolo

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,15 @@
+import spellchecker as sp
+
 class Dictionary:
     def __init__(self, lingua):
         self.lingua = lingua
         self._dict = []
+        self.spc=sp.SpellChecker()
 
     def loadDictionary(self, path):
         with open(path, "r", encoding='utf-8') as file:   #aggiungi l enncoding utf 8 per lo spagnolo
             for line in file:
-                self._dict.append(line)
+                self._dict.append(self.spc.replaceChars(line))  # #SE PASSI SOLO LINE VA BENE PER L ITALIANO E INGLESE VA MOLTO PIU VELOCE
 
     def printAll(self):
         for line in self._dict:
